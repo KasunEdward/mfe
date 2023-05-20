6ea9a64ee73333eda0b1642987fae20b9acc0eb1
Kasun Edward <kasunedward48@gmail.com>
2023-05-21 00:55:12 +0800

    Changethe yml to run on node v16

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -5,7 +5,7 @@ const packageJson = require('../package.json');
 
 const domain = process.env.PRODUCTION_DOMAIN;
 
-//prodConfig v1
+//prodConfig v2
 const prodConfig = {
     mode: 'production',
     output:{
